f92ffb1076a0674e6252434715c8555d4cbc205f
Leidy Johanna Carvajal Ortiz <iMuffins@macbook-pro-de-leidy.local>
2016-05-27 00:21:11 -0500

    Initial Commit

---
+++ TareasCurso/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TareasCurso
+//
+//  Created by Leidy Johanna Carvajal Ortiz on 27/05/16.
+//  Copyright © 2016 Leidy Johanna Carvajal Ortiz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
